141082c0ce9560d6b9a8273b90db53e36ac6c09d
Ewan Acemyan De Oliveira <127121274+WNCMYN@users.noreply.github.com>
2025-01-06 18:17:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ elem.addEventListener('click',function(){
     let youtube = document.querySelector('.youtube');
     let insta = document.querySelector('.insta');
     if ("styleV2sombre.css" === themeLink.getAttribute("href")) {
-        themeLink.setAttribute("href", "styleV2clair.css");
-        logo.setAttribute("src", "img/logo_blanc-modified.png");
-        destinations.setAttribute("src", "img/bouton_destination-modified.png");
-        contact.setAttribute("src", "img/bouton_contact-modified.png");
-        github.setAttribute("src", "img/github_blanc-modified.png");
-        youtube.setAttribute("src", "img/Youtube_logo-modified.png");
-        insta.setAttribute("src", "img/Instagram_logo-modified.png");
+        themeLink.setAttribute("href", "/styleV2clair.css");
+        logo.setAttribute("src", "/img/logo_blanc-modified.png");
+        destinations.setAttribute("src", "/img/bouton_destination-modified.png");
+        contact.setAttribute("src", "/img/bouton_contact-modified.png");
+        github.setAttribute("src", "/img/github_blanc-modified.png");
+        youtube.setAttribute("src", "/img/Youtube_logo-modified.png");
+        insta.setAttribute("src", "/img/Instagram_logo-modified.png");
     }
     else{
-        themeLink.setAttribute("href", "styleV2sombre.css");
-        logo.setAttribute("src", "img/logo_blanc.png");
-        destinations.setAttribute("src", "img/bouton_destination.png");
-        contact.setAttribute("src", "img/bouton_contact.png");
-        github.setAttribute("src", "img/github_blanc.png");
-        youtube.setAttribute("src", "img/Youtube_logo.png");
-        insta.setAttribute("src", "img/Instagram_logo.png");
+        themeLink.setAttribute("href", "/styleV2sombre.css");
+        logo.setAttribute("src", "/img/logo_blanc.png");
+        destinations.setAttribute("src", "/img/bouton_destination.png");
+        contact.setAttribute("src", "/img/bouton_contact.png");
+        github.setAttribute("src", "/img/github_blanc.png");
+        youtube.setAttribute("src", "/img/Youtube_logo.png");
+        insta.setAttribute("src", "/img/Instagram_logo.png");
     }
 }); 
